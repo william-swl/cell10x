0cf144f8ecc1160dd240617b49fb73dc5785df40
william <william_swl@163.com>
2023-05-07 11:38:09 +0800

    feat: modify smk_profile

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 current_dir=$(pwd)
 sed -i "s|/home/william/pipeline/cell10x|$current_dir|g" sample_config/test.yaml
+sed -i "s|/home/william/pipeline/cell10x|$current_dir|g" smk_profiles/cell10x.yaml
+sed -i "s|/home/william/pipeline/cell10x|$current_dir|g" smk_profiles/cell10x_slurm.yaml
