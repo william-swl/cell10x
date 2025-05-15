f9761ebef41ad275ca4223e7959b0b03ebf53215
william <william_swl@163.com>
2025-05-15 16:05:25 +0800

    feat: udpate igblast ref

--- src/igblast-ref/build-eg.sh
+++ src/igblast-ref/build-eg.sh
@@ -16,19 +16,3 @@ cp human_V.* ../../internal_data/human/
 
 # manual create igblast germline V gene annotation file
 # human.ndm.imgt for igblastn, imgt domain_system
-# human.pdm.imgt for igblastp, imgt domain_system
-
-
-
-# in germline_db/human-VDJT
-wget http://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/TR/TRAV.fasta
-wget http://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/TR/TRBV.fasta
-wget http://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/TR/TRDV.fasta
-wget http://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/TR/TRGV.fasta
-cat TRAV.fasta TRBV.fasta TRDV.fasta TRGV.fasta > TRV.fasta
-rm TRAV.fasta TRBV.fasta TRDV.fasta TRGV.fasta
-
-
-~/software/ncbi-igblast-1.17.1/bin/edit_imgt_file.pl TRV.fasta > human_TR_V
-~/software/ncbi-igblast-1.17.1/bin/makeblastdb -parse_seqids -dbtype nucl -in human_TR_V
-cp human_TR_V.* ../../internal_data/human/
